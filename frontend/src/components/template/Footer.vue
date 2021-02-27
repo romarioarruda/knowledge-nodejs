23ac81f707893246c5ed4cd417afb40f6b167216
Romário Arruda <romarioarruda98@gmail.com>
2021-02-27 15:59:34 -0300

    iniciando templates frontend

---
+++ frontend/src/components/template/Footer.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="footer">
+        <span>Copyright 
+            <strong id="boss">Romário Arruda</strong>
+            &copy; {{ new Date().getFullYear() }}
+        </span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Footer'
+}
+</script>
+
+<style>
+    .footer {
+        grid-area: footer;
+        background-color: #ddd;
+        color: #333;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding-right: 10px;
+    }
+
+    #boss{
+        color: red;
+    }
+</style>
