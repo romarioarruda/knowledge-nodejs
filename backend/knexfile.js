cf5080e3492585695238b7ed49ff1cc55b767b5e
Romário Arruda <romarioarruda98@gmail.com>
2021-02-27 14:48:07 -0300

    adicionando informações de conexao no .env

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,10 +1,8 @@
+const { db } = require('./.env')
+
 module.exports = {
   client: 'postgresql',
-  connection: {
-    database: 'knowledge_final',
-    user: 'postgres',
-    password: ''
-  },
+  connection: db,
   pool: {
     min: 2,
     max: 10
